d29163cf10fbf12ed35612fbb93b38f0a7892b58
Ruben Jacobse <32992187+RubenJacobse@users.noreply.github.com>
2019-04-22 02:19:46 +0200

    Restructured tools/__init__.py

--- bluesky/tools/__init__.py
+++ bluesky/tools/__init__.py
@@ -1,19 +1,26 @@
-from bluesky import settings
-# Register settings defaults
-settings.set_variable_defaults(prefer_compiled=False)
+""" Tools package imports """
+
+# Make available in the BlueSky package structure
+from . import cachefile
 from .walltime import Timer
 from .trafficarrays import RegisterElementParameters, TrafficArrays
 from .signal import Signal
+
+# Register default configuration variable
+from bluesky import settings
+settings.set_variable_defaults(prefer_compiled=False)
+
+# Import geo calculation module. Use the compiled version if preferred,
+# otherwise revert back to the Python version. This is hidden from the caller
+# which imports the selected module using 'from bluesky.tools import geo'.
+geo = None
 if settings.prefer_compiled:
     try:
-        from . import cgeo as geo
-        print('Using compiled geo functions')
+        from .src_cpp import cgeo as geo
+        print("Using compiled geo functions.")
     except ImportError:
-        from . import geo
-        print('Using Python-based geo functions')
-else:
-    from . import geo
-    print('Using Python-based geo functions')
+        print("No compiled geo functions for this platform.")
 
-from .trafficarrays import RegisterElementParameters, TrafficArrays
-from . import cachefile
+if not geo:
+    from . import geo
+    print("Using Python-based geo functions")
