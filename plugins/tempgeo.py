8fbed6e83862fa6f038be9cefc87af81922e7fc4
Ruben Jacobse <32992187+RubenJacobse@users.noreply.github.com>
2019-04-25 01:49:21 +0200

    Added great circle intersect calculation to tempgeo

--- plugins/tempgeo.py
+++ plugins/tempgeo.py
@@ -12,6 +12,8 @@ by arctan2(y,x) to return angles in the correct quadrant.
 # Third-party imports
 import numpy as np
 
+# Module constants 
+ITER_CALC_LAT_MARGIN = 1e-6
 
 def rhumb_azimuth(lat_start, lon_start, lat_end, lon_end):
     """
@@ -113,3 +115,192 @@ def natlog_factor(lat):
         ** (eccentricity / 2)
 
     return np.log(tan_term * lat_term)
+
+
+def sphere_greatcircle_intersect(p1_lat, p1_lon, p2_lat, p2_lon, p3_lat, p3_lon, p4_lat, p4_lon):
+    """
+    Calculate the intersection point of two great circle arcs, the
+    first defined by points: (p1, p2) and the second defined by the
+    points: (p3, p4).
+
+    Returns (lat, lon) of the intersection point if it exists, and
+    raises a ValueError if the arcs do not intersect.
+    """
+
+    def latlon_to_unitvector(lat, lon):
+        """
+        Convert a point defined by a pair of (lat, lon) coordinates on
+        the surface of a sphere to a three-dimensional unit vector in
+        cartesian coordinates (x, y, z).
+
+        Returns a one-dimensional numpy array containing [x, y, z].
+        """
+
+        x = np.cos(np.radians(lon)) * np.cos(np.radians(lat))
+        y = np.sin(np.radians(lon)) * np.cos(np.radians(lat))
+        z = np.sin(np.radians(lat))
+
+        return np.array([x, y, z])
+
+    def unitvector_to_latlon(cart_x, cart_y, cart_z):
+        """
+        Convert a point on the unit sphere defined by a unit vector with
+        cartesian coordinates (x, y, z) to a pair of (lat, lon) coordinates
+        on the sphere surface.
+        """
+
+        sphere_lon = np.arctan2(cart_y, cart_x)
+        sphere_lat = np.arctan2(cart_z, np.sqrt(cart_x ** 2 + cart_y ** 2))
+
+        return sphere_lat, sphere_lon
+
+    # Convert each point to a unit vector
+    vec_a0 = latlon_to_unitvector(p1_lat, p1_lon)
+    vec_a1 = latlon_to_unitvector(p2_lat, p2_lon)
+    vec_b0 = latlon_to_unitvector(p3_lat, p3_lon)
+    vec_b1 = latlon_to_unitvector(p4_lat, p4_lon)
+
+    # Calculate normal vectors to the planes defining arcs a and b
+    vec_p = np.cross(vec_a0, vec_a1)
+    vec_q = np.cross(vec_b0, vec_b1)
+
+    # Calculate normalized vector along the line of plane intersection
+    vec_t = np.cross(vec_p, vec_q)
+    unit_t = vec_t
+
+    # Projections of t along arcs a and b
+    s_1 = np.dot(np.cross(vec_a0, vec_p), unit_t)
+    s_2 = np.dot(np.cross(vec_a1, vec_p), unit_t)
+    s_3 = np.dot(np.cross(vec_b0, vec_q), unit_t)
+    s_4 = np.dot(np.cross(vec_b1, vec_q), unit_t)
+
+    # Test if arcs intersect and if so, determine at which location
+    if -s_1 < 0 and s_2 < 0 and -s_3 < 0 and s_4 < 0:
+        # Intersection occurs at -t
+        intersect_t = -1 * unit_t
+    elif -s_1 >= 0 and s_2 >= 0 and -s_3 >= 0 and s_4 >= 0:
+        # Intersection occurs at t
+        intersect_t = unit_t
+    else:
+        # No intersection:
+        raise ValueError("Great circle arcs do not intersect at any point.")
+
+    # Convert intersect_t back to lat, lon
+    intersect_x = intersect_t[0]
+    intersect_y = intersect_t[1]
+    intersect_z = intersect_t[2]
+
+    intersect_lat, intersect_lon = unitvector_to_latlon(intersect_x, intersect_y, intersect_z)
+
+    return np.degrees(intersect_lat), np.degrees(intersect_lon)
+
+
+def ellipsoid_greatcircle_intersect(p1_lat, p1_lon, p2_lat, p2_lon, p3_lat, p3_lon, p4_lat, p4_lon):
+    """
+    Calculate the intersection point of two great circle arcs, the
+    first defined by points: (p1, p2) and the second defined by the
+    points: (p3, p4).
+
+    Returns (lat, lon) of the intersection point if it exists, and
+    raises a ValueError if the arcs do not intersect.
+
+    Equations adapted from:
+    https://gssc.esa.int/navipedia/index.php/Ellipsoidal_and_Cartesian_Coordinates_Conversion
+    """
+
+    def latlon_to_vector(lat, lon):
+        """
+        Convert a point defined by a pair of (lat, lon) coordinates on
+        the surface of an ellipsoid to a three-dimensional unit vector in
+        cartesian coordinates (x, y, z).
+
+        Returns a one-dimensional numpy array containing [x, y, z].
+        """
+
+        e = 0.08181919084262149  # [-] WGS84 Earth eccentricity factor
+
+        # Radius of curvature in prime vertical
+        N = 1 / np.sqrt(1 - (e ** 2 * np.sin(np.radians(lat)) ** 2))
+
+        # Calculate cartesian coordinates and normalize to unit vector
+        cart_x = N * np.cos(np.radians(lat)) * np.cos(np.radians(lon))
+        cart_y = N * np.cos(np.radians(lat)) * np.sin(np.radians(lon))
+        cart_z = (1 - e ** 2) * N * np.sin(np.radians(lat))
+
+        cart_vec = np.array([cart_x, cart_y, cart_z])
+        cart_unit_vec = cart_vec / np.linalg.norm(cart_vec)
+
+        return cart_unit_vec
+        # return np.array([cart_x, cart_y, cart_z])
+
+    def vector_to_latlon(cart_x, cart_y, cart_z):
+        """
+        Convert a point on the unit sphere defined by a unit vector with
+        cartesian coordinates (x, y, z) to a pair of (lat, lon) coordinates
+        on the ellipsoid surface.
+        """
+
+        e = 0.08181919084262149  # [-] WGS84 Earth eccentricity factor
+
+        p = np.sqrt(cart_x ** 2 + cart_y ** 2)
+
+        # Finding longitude is simple
+        ellipse_lon = np.arctan2(cart_y, cart_x)
+
+        # For latitude: iterate until successive latitude values converge within margin
+        lat_init = np.arctan2(cart_z, (1 - e ** 2) * p)
+
+        lat_prev = lat_init
+        while True:
+            N = 1 / np.sqrt(1 - (e ** 2) * (np.sin(lat_prev) ** 2) )
+            h = (p / np.cos(lat_prev)) - N
+
+            lat_curr = np.arctan2(cart_z, (1 - (e ** 2) * (N / (N + h))) * p)
+
+            if abs(lat_curr - lat_prev) < ITER_CALC_LAT_MARGIN:
+                ellipse_lat = lat_curr
+                break
+            else:
+                lat_prev = lat_curr
+
+        return ellipse_lat, ellipse_lon
+
+    # Convert each point to a unit vector
+    vec_a0 = latlon_to_vector(p1_lat, p1_lon)
+    vec_a1 = latlon_to_vector(p2_lat, p2_lon)
+    vec_b0 = latlon_to_vector(p3_lat, p3_lon)
+    vec_b1 = latlon_to_vector(p4_lat, p4_lon)
+
+    # Calculate normal vectors to the planes defining arcs a and b
+    vec_p = np.cross(vec_a0, vec_a1)
+    vec_q = np.cross(vec_b0, vec_b1)
+
+    # Calculate normalized vector along the line of plane intersection
+    vec_t = np.cross(vec_p, vec_q)
+    unit_t = vec_t / np.linalg.norm(vec_t)
+
+    # Projections of t along arcs a and b
+    s_1 = np.dot(np.cross(vec_a0, vec_p), unit_t)
+    s_2 = np.dot(np.cross(vec_a1, vec_p), unit_t)
+    s_3 = np.dot(np.cross(vec_b0, vec_q), unit_t)
+    s_4 = np.dot(np.cross(vec_b1, vec_q), unit_t)
+
+    # Test if arcs intersect and if so, determine at which location
+    if -s_1 < 0 and s_2 < 0 and -s_3 < 0 and s_4 < 0:
+        # Intersection occurs at -t
+        intersect_t = -1 * unit_t
+    elif -s_1 >= 0 and s_2 >= 0 and -s_3 >= 0 and s_4 >= 0:
+        # Intersection occurs at t
+        intersect_t = unit_t
+    else:
+        # No intersection:
+        raise ValueError("Great circle arcs do not intersect at any point.")
+
+    # Convert intersect_t back to lat, lon
+    intersect_x = intersect_t[0]
+    intersect_y = intersect_t[1]
+    intersect_z = intersect_t[2]
+
+    intersect_lat, intersect_lon = vector_to_latlon(intersect_x, intersect_y, intersect_z)
+
+    return np.degrees(intersect_lat), np.degrees(intersect_lon)
