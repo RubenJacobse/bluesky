a725347eeddd311bc9410fc3cb0504e8c3b9b606
Ruben Jacobse <32992187+RubenJacobse@users.noreply.github.com>
2019-05-16 21:15:54 +0200

    Added script to generate multiple scenario files

---
+++ thesis_ruben/generate_multiple.py
@@ -0,0 +1,17 @@
+"""
+Bla
+"""
+
+import itertools
+from scenario_generator import create
+
+corridor_length = [10, 20, 30, 40, 50]
+corridor_width = [5, 10, 15, 20, 25]
+ac_creation_arc_angle = [80, 90, 100, 110, 120]
+asas_reso_method = ["OFF", "LF", "MVP"]
+
+combination_lst = list(itertools.product(corridor_length, corridor_width,
+                                         ac_creation_arc_angle, asas_reso_method))
+
+for (length, width, angle, reso_method) in combination_lst:
+    create(reso_method, length, width, angle, is_restriction_angle=True)
