8a585288ebefc440472f110f496e939a2f30d209
Ruben Jacobse <32992187+RubenJacobse@users.noreply.github.com>
2019-10-29 19:47:49 +0100

    Remove traf partial conversion to struct type

--- bluesky/traffic/asas/src_c/StateBasedCD.pyx
+++ bluesky/traffic/asas/src_c/StateBasedCD.pyx
@@ -7,29 +7,17 @@ import numpy as np
 cimport numpy as np
 cimport cgeo
 
+ctypedef np.float32_t DTYPE_t
+
 cdef double RAD_TO_DEG = 180 / pi
 cdef double DEG_TO_RAD = pi / 180
 cdef double nm = 1852.0
-
-ctypedef np.float32_t DTYPE_t
-
-cdef struct traf:
-    int ntraf
-    double *lat
-    double *lon
-    double *trk
-    double *gs
-    double *alt
-    double *vs
-
-ctypedef traf traf_arr
 
 
 cpdef detect(ownship, intruder, RPZ, HPZ, tlookahead):
     """ Conflict detection between ownship (traf) and intruder (traf/adsb)."""
 
     # Declare C type variables to which Python variables will be cast
-    cdef traf_arr ownship_, intruder_
     cdef double pz_radius, pz_height, t_lookahead
 
     # Ensure typecast to double types
@@ -37,96 +25,85 @@ cpdef detect(ownship, intruder, RPZ, HPZ, tlookahead):
     pz_height = <double> HPZ
     t_lookahead = <double> tlookahead
 
-    # Store traffic variables in struct type
-    ownship_ = convert_to_struct(ownship)
-    intruder_ = convert_to_struct(intruder)
-
     # Call actual detection function
     (confidxpairs, losidxpairs, inconf, tcpamax, conf_qdr, conf_dist, conf_dcpa,
-     conf_tcpa, conf_tinconf) = detect_all(ownship_, intruder_, pz_radius,
+     conf_tcpa, conf_tinconf) = detect_all(ownship, intruder, pz_radius,
                                            pz_height, t_lookahead)
 
-    confpairs = [(ownship.id[i], ownship.id[j]) for (i, j) in confidxpairs]
-    lospairs = [(ownship.id[i], ownship.id[j]) for (i, j) in losidxpairs]
+    confpairs = [(ownship.id[ii], intruder.id[jj]) for (ii, jj) in confidxpairs]
+    lospairs = [(ownship.id[ii], intruder.id[jj]) for (ii, jj) in losidxpairs]
 
     return (confpairs, lospairs, inconf, tcpamax, conf_qdr, conf_dist,
             conf_dcpa, conf_tcpa, conf_tinconf)
-
-@cython.boundscheck(False)
-@cython.wraparound(False)
-cdef traf_arr convert_to_struct(traf_obj):
-    """ 
-    Extract the relevant parameters of the traf object to a 
-    traf_arr struct.
-    """
-
-    cdef traf_arr arr
-
-    # Convert numpy arrays to memoryviews
-    cdef double[::1] lat = np.ascontiguousarray(traf_obj.lat)
-    cdef double[::1] lon = np.ascontiguousarray(traf_obj.lon)
-    cdef double[::1] trk = np.ascontiguousarray(traf_obj.trk)
-    cdef double[::1] gs = np.ascontiguousarray(traf_obj.gs)
-    cdef double[::1] alt = np.ascontiguousarray(traf_obj.alt)
-    cdef double[::1] vs = np.ascontiguousarray(traf_obj.vs)
-
-    # Store data in struct
-    arr.ntraf = <int> traf_obj.ntraf
-    arr.lat = &lat[0]
-    arr.lon = &lon[0]
-    arr.trk = &trk[0]
-    arr.gs = &gs[0]
-    arr.alt = &alt[0]
-    arr.vs = &vs[0]
-
-    return arr
 
 
 @cython.cdivision(True)
 @cython.boundscheck(False)
 @cython.wraparound(False)
-cdef detect_all(traf_arr ownship,
-                traf_arr intruder,
+cdef detect_all(ownship,
+                intruder,
                 double pz_radius,
                 double pz_height,
                 double t_lookahead):
 
+    # Convert ownship variables to memoryviews
+    cdef int ownship_ntraf = <int> ownship.ntraf
+    cdef double[::1] ownship_lat = np.ascontiguousarray(ownship.lat)
+    cdef double[::1] ownship_lon = np.ascontiguousarray(ownship.lon)
+    cdef double[::1] ownship_trk = np.ascontiguousarray(ownship.trk)
+    cdef double[::1] ownship_gs = np.ascontiguousarray(ownship.gs)
+    cdef double[::1] ownship_alt = np.ascontiguousarray(ownship.alt)
+    cdef double[::1] ownship_vs = np.ascontiguousarray(ownship.vs)
+
+    # Convert intruder variables to memoryviews
+    cdef int intruder_ntraf = <int> intruder.ntraf
+    cdef double[::1] intruder_lat = np.ascontiguousarray(intruder.lat)
+    cdef double[::1] intruder_lon = np.ascontiguousarray(intruder.lon)
+    cdef double[::1] intruder_trk = np.ascontiguousarray(intruder.trk)
+    cdef double[::1] intruder_gs = np.ascontiguousarray(intruder.gs)
+    cdef double[::1] intruder_alt = np.ascontiguousarray(intruder.alt)
+    cdef double[::1] intruder_vs = np.ascontiguousarray(intruder.vs)
+
+    # Prepare fixed-size arrays with one element for each aircraft
+    cdef np.ndarray[int, ndim = 1] inconf = np.zeros((ownship_ntraf), dtype=int)
+    cdef np.ndarray[DTYPE_t, ndim=1] tcpamax = np.zeros((ownship_ntraf), dtype=np.float32)
+
+    # Dynamically sized variables to store data for each conflict pair
+    confpairs = []
+    conf_qdr = np.zeros((0), dtype=np.float32)
+    conf_dist = np.zeros((0), dtype=np.float32)
+    conf_tcpa = np.zeros((0), dtype=np.float32)
+    conf_dcpa = np.zeros((0), dtype=np.float32)
+    conf_tinconf = np.zeros((0), dtype=np.float32)
+
+    # Dynamically sized list to store each loss of separation pair
+    lospairs = []
+
+    # Declare variables used inside double loop
     cdef double qdr, dist, dx, dy, owntrkrad, ownu, ownv, inttrkrad, intu, \
     intv, du, dv, dv2, vrel, tcpa, dcpa2, R2, dxinhor, dtinhor, tinhor, \
     touthor, dalt, dvs, tcrosslo, tcrosshi, tinver, toutver, tinconf, toutconf
-    cdef int is_hor_conf, is_conf, is_los
+    cdef int is_hor_conf, is_conf, is_los, ii, jj
 
-    # Fixed-size array with one element for each aircraft
-    cdef np.ndarray[int, ndim = 1] inconf = np.zeros((ownship.ntraf), dtype=int)
-    cdef np.ndarray[DTYPE_t, ndim=1] tcpamax = np.zeros((ownship.ntraf), dtype=np.float32)
-
-    # Dynamically sized based on number of pairs in conflict
-    cdef list confpairs = []
-    cdef np.ndarray[DTYPE_t, ndim=1] conf_qdr = np.zeros((0), dtype=np.float32)
-    cdef np.ndarray[DTYPE_t, ndim=1] conf_dist = np.zeros((0), dtype=np.float32)
-    cdef np.ndarray[DTYPE_t, ndim=1] conf_tcpa = np.zeros((0), dtype=np.float32)
-    cdef np.ndarray[DTYPE_t, ndim=1] conf_dcpa = np.zeros((0), dtype=np.float32)
-    cdef np.ndarray[DTYPE_t, ndim=1] conf_tinconf = np.zeros((0), dtype=np.float32)
-
-    # Dynamically sized based on number of pairs with loss of separation
-    cdef list lospairs = []
-
-    cdef int ii, jj
-    for ii in range(ownship.ntraf):
-        for jj in range(intruder.ntraf):
+    # Check all ownship-intruder combinations for conflicts and
+    # loss of separation
+    for ii in range(ownship_ntraf):
+        for jj in range(intruder_ntraf):
             # Skip conflict with self
             if ii == jj:
                 continue
-
+ 
+            print(f"{ii},{jj}")
             # Horizontal conflict ----------------------------------------------
-            qdr = cgeo.cy_qdr(ownship.lat[ii], 
-                              ownship.lon[ii],
-                              intruder.lat[jj],
-                              intruder.lon[jj])
-            dist = cgeo.cy_dist(ownship.lat[ii], 
-                                ownship.lon[ii],
-                                intruder.lat[jj],
-                                intruder.lon[jj])
+            qdr = cgeo.cy_qdr(ownship_lat[ii], 
+                              ownship_lon[ii],
+                              intruder_lat[jj],
+                              intruder_lon[jj])
+            dist = cgeo.cy_dist(ownship_lat[ii], 
+                                ownship_lon[ii],
+                                intruder_lat[jj],
+                                intruder_lon[jj])
+            print(f"\tqdr: {qdr} dist:{dist}")
 
             # Convert distance to m and qdr to radians
             dist = dist * nm
@@ -137,14 +114,14 @@ cdef detect_all(traf_arr ownship,
             dy = dist * cos(qdrrad)
 
             # Ownship track angle and speed
-            owntrkrad = ownship.trk[ii] * DEG_TO_RAD
-            ownu = ownship.gs[ii] * sin(owntrkrad)
-            ownv = ownship.gs[ii] * cos(owntrkrad)
+            owntrkrad = ownship_trk[ii] * DEG_TO_RAD
+            ownu = ownship_gs[ii] * sin(owntrkrad)
+            ownv = ownship_gs[ii] * cos(owntrkrad)
 
             # Intruder track angle and speed
-            inttrkrad = intruder.trk[jj] * DEG_TO_RAD
-            intu = intruder.gs[jj] * sin(inttrkrad)
-            intv = intruder.gs[jj] * cos(inttrkrad)
+            inttrkrad = intruder_trk[jj] * DEG_TO_RAD
+            intu = intruder_gs[jj] * sin(inttrkrad)
+            intv = intruder_gs[jj] * cos(inttrkrad)
 
             # Relative velocities
             du = ownu - intu
@@ -167,12 +144,13 @@ cdef detect_all(traf_arr ownship,
             else:
                 tinhor = 1e8
                 touthor = -1e8
+            print(f"is_hor_conf: {is_hor_conf}")
 
             # Vertical crossing of disk (-dh,+dh)
-            dalt = ownship.alt[ii] - intruder.alt[jj]
-            dvs = ownship.vs[ii] - ownship.vs[jj]
+            dalt = ownship_alt[ii] - intruder_alt[jj]
+            dvs = ownship_vs[ii] - intruder_vs[jj]
 
-            if fabs(dvs) < 1e-6:
+            if fabs(dvs) < 1e-6: 
                 dvs = 1e-6
 
             tcrosslo = (dalt - pz_height) / -dvs
@@ -190,7 +168,7 @@ cdef detect_all(traf_arr ownship,
 
             if is_conf:
                 confpairs.append((ii, jj))
-                inconf[ii] = True
+                inconf[ii] = 1
                 if tcpa > tcpamax[ii]:
                     tcpamax[ii] = tcpa
                 conf_qdr = np.append(conf_qdr, [qdr])
