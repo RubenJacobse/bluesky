55d7052d5a6670b7713b1c52a46917160de6ec42
Ruben Jacobse <32992187+RubenJacobse@users.noreply.github.com>
2019-11-14 01:04:41 +0100

    Remove possibility of asas overriding area avoidance in wrong direction

--- plugins/thesis/restriction_manager.py
+++ plugins/thesis/restriction_manager.py
@@ -613,14 +613,5 @@ class AreaRestrictionManager(TrafficArrays):
         wp_dest_crs = self.crs_to_dest_wp[ac_indices]
         avoidance_crs = tg.crs_closest(wp_dest_crs, crs_left, crs_right)
 
-        # Follow ASAS resolutions which do not result in area conflicts
-        asas_active = bs.traf.asas.active[ac_indices]
-        pilot_crs = bs.traf.pilot.hdg[ac_indices]
-        free_of_conflict = np.array([not tg.crs_is_between(crs, crs_l, crs_r)
-                                     for crs, crs_l, crs_r
-                                     in zip(pilot_crs, crs_left, crs_right)])
-        avoidance_crs = np.where(asas_active & free_of_conflict,
-                                 pilot_crs, avoidance_crs)
-
         # Set new courses in the traffic object
         self.hdg[ac_indices] = avoidance_crs
