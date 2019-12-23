f1dd0bbc7cddbc357147992e8117c149487a88ec
Ruben Jacobse <32992187+RubenJacobse@users.noreply.github.com>
2019-12-23 21:05:22 +0100

    Update implementation of Eby resolution method

--- bluesky/traffic/asas/Eby.py
+++ bluesky/traffic/asas/Eby.py
@@ -4,6 +4,7 @@ Created on Tue Mar 03 16:50:19 2015
 
 @author: Jerom Maas
 """
+
 import numpy as np
 from bluesky.tools.aero import vtas2eas
 
@@ -13,77 +14,88 @@ def start(asas):
 
 
 def resolve(asas, traf):
-    if not asas.swasas:
-        return
+    """ Resolve all current conflicts """
 
-    # required change in velocity
-    dv = np.zeros((traf.ntraf, 3))
+    # Initialize an array to store the resolution velocity vector for all A/C
+    delta_v = np.zeros((traf.ntraf, 3))
 
-    #if possible, solve conflicts once and copy results for symmetrical conflicts,
-    #if that is not possible, solve each conflict twice, once for each A/C
-    if not traf.ADSBtrunc and not traf.ADSBtransnoise:
-        for conflict in asas.conflist_now:
-            id1, id2 = asas.ConflictToIndices(conflict)
-            if id1 != "Fail" and id2 != "Fail":
-                dv_eby = Eby_straight(traf, asas, id1, id2)
-                dv[id1] -= dv_eby
-                dv[id2] += dv_eby
-    else:
-        for i in range(asas.nconf):
-            confpair = asas.confpairs[i]
-            ac1      = confpair[0]
-            ac2      = confpair[1]
-            id1      = traf.id.index(ac1)
-            id2      = traf.id.index(ac2)
-            dv_eby   = Eby_straight(asas, id1, id2)
-            dv[id1] -= dv_eby
+    # Stores resolution vector, also used in visualization
+    asas.asasn = np.zeros(traf.ntraf, dtype=np.float32)
+    asas.asase = np.zeros(traf.ntraf, dtype=np.float32)
 
-    # now we have the change in speed vector for each aircraft.
-    dv=np.transpose(dv)
-    # the old speed vector, cartesian coordinates
-    trkrad=np.radians(traf.trk)
-    v=np.array([np.sin(trkrad)*traf.tas,\
-        np.cos(trkrad)*traf.tas,\
-        traf.vs])
-    # the new speed vector
-    newv=dv+v
+    # Call Eby function to resolve conflicts
+    for (ac1, ac2), qdr, dist in zip(asas.confpairs, asas.qdr, asas.dist):
+        idx1 = traf.id2idx(ac1)
+        idx2 = traf.id2idx(ac2)
 
-    # the new speed vector in polar coordinates
-    newtrack=(np.arctan2(newv[0,:],newv[1,:])*180/np.pi) %360
-    newgs=np.sqrt(newv[0,:]**2+newv[1,:]**2)
-    neweas=vtas2eas(newgs,traf.alt)
+        # Do not resolve current pair if:
+        # - the pair contains an aircraft that no longer exists
+        # - resolutions are switched off for ac1
+        # - ac2 is a noreso aircraft which ac1 should not avoid
+        if (not (idx1 > -1 and idx2 > -1)
+                or (asas.swresooff and ac1 in asas.resoofflst)
+                or (asas.swnoreso and ac2 in asas.noresolst)):
+            continue
+
+        dv_eby = Eby(traf, asas, qdr, dist, idx1, idx2)
+        delta_v[idx1] -= dv_eby
+
+    # Determine new speed and limit resolution direction for all aicraft-------
+
+    # Resolution vector for all aircraft, cartesian coordinates
+    delta_v = np.transpose(delta_v)
+
+    # The old speed vector, cartesian coordinates
+    v = np.array([traf.gseast, traf.gsnorth, traf.vs])
+
+    # The new speed vector, cartesian coordinates
+    new_v = delta_v + v
+
+    # Get indices of aircraft that have a resolution
+    idxs = delta_v[0, :]**2 + delta_v[1, :]**2 > 0
+
+    # The new speed vector in polar coordinates
+    new_track = np.degrees(np.arctan2(new_v[0, :], new_v[1, :])) % 360
+    new_gs = np.sqrt(new_v[0, :]**2 + new_v[1, :]**2)
+    new_vs = new_v[2, :]
+
+    # Determine ASAS module commands for all aircraft--------------------------
 
     # Cap the velocity
-    neweascapped=np.maximum(asas.vmin,np.minimum(asas.vmax,neweas))
+    new_gs_capped = np.clip(new_gs, asas.vmin, asas.vmax)
 
-    # now assign in the traf class
-    asas.hdg = newtrack
-    asas.tas = neweascapped
-    asas.vs  = newv[2,:]
-    asas.alt = np.sign(asas.vs)*1e5
+    # Now assign resolutions to variables in the ASAS class
+    asas.trk = new_track
+    asas.tas = new_gs_capped
+    asas.vs = new_vs
+    asas.alt = traf.alt #np.sign(asas.vs) * 1e5
 
-#=================================== Eby Method ===============================
+    # Stores resolution vector
+    asas.asase[idxs] = asas.tas[idxs] * np.sin(asas.trk[idxs] / 180 * np.pi)
+    asas.asasn[idxs] = asas.tas[idxs] * np.cos(asas.trk[idxs] / 180 * np.pi)
+    # asaseval should be set to True now
+    if not asas.asaseval:
+        asas.asaseval = True
 
-    # Resolution: Eby method assuming aircraft move straight forward, solving algebraically, only horizontally
-def Eby_straight(traf, asas, id1, id2):
-    traf = traf
-    dist = asas.dist[id1,id2]
-    qdr  = asas.qdr[id1,id2]
-    # from degrees to radians
-    qdr  = np.radians(qdr)
-    # relative position vector
-    d    = np.array([np.sin(qdr)*dist, \
-           np.cos(qdr)*dist, \
-           traf.alt[id2]-traf.alt[id1] ])
 
-    # find track in radians
-    t1 = np.radians(traf.trk[id1])
-    t2 = np.radians(traf.trk[id2])
+def Eby(traf, asas, qdr, dist, idx1, idx2):
+    """
+    Eby Voltage potential resolution method.
+    """
 
-    # write velocities as vectors and find relative velocity vector
-    v1=np.array([np.sin(t1)*traf.tas[id1],np.cos(t1)*traf.tas[id1],traf.vs[id1]])
-    v2=np.array([np.sin(t2)*traf.tas[id2],np.cos(t2)*traf.tas[id2],traf.vs[id2]])
-    v=np.array(v2-v1)
+    # Convert qdr from degrees to radians
+    qdr = np.radians(qdr)
+
+    # Relative position vector between idx1 and idx2
+    drel = np.array([np.sin(qdr) * dist,
+                     np.cos(qdr) * dist,
+                     traf.alt[idx2] - traf.alt[idx1]])
+
+    # Write velocities as vectors and find relative velocity vector
+    v1 = np.array([traf.gseast[idx1], traf.gsnorth[idx1], traf.vs[idx1]])
+    v2 = np.array([traf.gseast[idx2], traf.gsnorth[idx2], traf.vs[idx2]])
+    vrel = np.array(v2 - v1)
+
     # bear in mind: the definition of vr (relative velocity) is opposite to
     # the velocity vector in the LOS_nominal method, this just has consequences
     # for the derivation of tstar following Eby method, not more
@@ -99,40 +111,45 @@ def Eby_straight(traf, asas, id1, id2):
     -Solve using the quadratic formula
     """
     # These terms are used to construct a,b,c of the quadratic formula
-    R2=asas.Rm**2 # in meters
-    d2=np.dot(d,d) # distance vector length squared
-    v2=np.dot(v,v) # velocity vector length squared
-    dv=np.dot(d,v) # dot product of distance and velocity
+    R2 = asas.Rm**2 # in meters
+    d2 = np.dot(drel, drel) # distance vector length squared
+    v2 = np.dot(vrel, vrel) # velocity vector length squared
+    dv = np.dot(drel, vrel) # dot product of distance and velocity
 
     # Solving the quadratic formula
-    a=R2*v2 - dv**2
-    b=2*dv* (R2 - d2)
-    c=R2*d2 - d2**2
-    discrim=b**2 - 4*a*c
+    a = R2 * v2 - dv**2
+    b = 2 * dv * (R2 - d2)
+    c = R2 * d2 - d2**2
+    discrim = b**2 - 4 * a * c
 
-    if discrim<0: # if the discriminant is negative, we're done as taking the square root will result in an error
-        discrim=0
-    time1=(-b+np.sqrt(discrim))/(2*a)
-    time2=(-b-np.sqrt(discrim))/(2*a)
+    # If the discriminant is negative, we're done as taking the square root
+    # will result in an error
+    if discrim < 0:
+        discrim = 0
+    time1 = ( -b + np.sqrt(discrim)) / (2 * a)
+    time2 = ( -b - np.sqrt(discrim)) / (2 * a)
 
-    #time when the size of the conflict is largest relative to time to solve
-    tstar=min(abs(time1),abs(time2))
+    # Time when the size of the conflict is largest relative to time to solve
+    tstar = min(abs(time1), abs(time2))
 
-    #find drel and absolute distance at tstar
-    drelstar=d+v*tstar
-    dstarabs=np.linalg.norm(drelstar)
-    #exception: if the two aircraft are on exact collision course
-    #(passing eachother within 10 meter), change drelstar
-    exactcourse=10 #10 meter
-    dif=exactcourse-dstarabs
-    if dif>0:
-        vperp=np.array([-v[1],v[0],0]) #rotate velocity 90 degrees in horizontal plane
-        drelstar+=dif*vperp/np.linalg.norm(vperp) #normalize to 10 m and add to drelstar
-        dstarabs=np.linalg.norm(drelstar)
+    # Find drel and absolute distance at tstar
+    drelstar = drel + vrel * tstar
+    dstarabs = np.linalg.norm(drelstar)
 
-    #intrusion at tstar
-    i=asas.Rm-dstarabs
+    # Exception: if the two aircraft are on exact collision course
+    # (passing eachother within 10 meter), change drelstar
+    exactcourse = 10 # 10 meter
+    dif = exactcourse - dstarabs
+    if dif > 0:
+        # rotate velocity 90 degrees in horizontal plane
+        vperp = np.array([-vrel[1], vrel[0], 0])
+        # normalize to 10 m and add to drelstar
+        drelstar += dif * vperp / np.linalg.norm(vperp)
+        dstarabs = np.linalg.norm(drelstar)
 
-    #desired change in the plane's speed vector:
-    dv=i*drelstar/(dstarabs*tstar)
-    return dv
+    # intrusion at tstar
+    i = asas.Rm - dstarabs
+
+    # desired change in the plane's speed vector:
+    delta_v = i * drelstar / (dstarabs * tstar)
+    return delta_v
